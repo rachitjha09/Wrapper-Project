4fc0ff93b5d3ec90c07e12162ee42cdc78cf2c01
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 19:40:34 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+
+/**
+ * Demo Wrapper Class
+ *
+ * @author (Rachit Jha)
+ * @version 10/13/23)
+ */
+public class Demo
+{
+    public static void main(String[]args){
+        int n=123;
+        String nAsAStrin=n+ "";
+        String str="123abc";
+        
+    }
+}
